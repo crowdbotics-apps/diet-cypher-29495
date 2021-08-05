d8f960d1ea3300aae472c0b8ad7ce94ea164e094
Crowdbotics <team@crowdbotics.com>
2021-08-05 04:37:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/diet_cypher_29495/MainActivity.java
@@ -0,0 +1,15 @@
+package com.diet_cypher_29495;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "diet_cypher_29495";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "diet_cypher_29495Identifier",
+  url: "https://diet_cypher_29495Identifier.botics.co",
+  api: "https://diet_cypher_29495Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
